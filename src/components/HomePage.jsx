a01d0427003f88434b8396d44704837cd06720d4
Hillcrest01 <113606557+Hillcrest01@users.noreply.github.com>
2024-07-26 11:06:15 +0300

    Add files via upload

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../styles.css';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://egerton-freemarket-90oc.onrender.com/products')
+            .then(response => setProducts(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div className="container">
+            <hr />
+            <div className="product-grid">
+                {products.map(product => (
+                    <div key={product.id} className="card">
+                        <div className="card-content">
+                        <Link to={`/products/${product.id}`}><img src={`https://egerton-freemarket-90oc.onrender.com/uploads/${product.image_filename}`} alt={product.name} /></Link>
+                        <Link to={`/products/${product.id}`}><h2>{product.name}</h2></Link>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                        </div>
+                        <div className="card-actions">
+                        <a href={`tel:${product.contact_number}`}>
+                            <button>Call</button>
+                        </a>
+                        <a href={`https://wa.me/${product.contact_number}`}>
+                            <button>WhatsApp</button>
+                        </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
